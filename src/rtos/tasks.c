cf744c0d23e4549cd02cf236f3f0669fec6852c3
Nguyen Thien Duc <ntduc@pm.me>
2026-09-18 03:58:08 +0800

    init lists

--- src/rtos/tasks.c
+++ src/rtos/tasks.c
@@ -1,14 +1,20 @@
 #include <stdint.h>
 
 #include "cmsis_gcc.h"
+#include "rtos/list.h"
 #include "rtos/ports/rtos_port.h"
 #include "tasks.h"
 
-static rtos_tcb_t task_pool[RTOS_MAX_TASKS];
-static uint32_t task_count;
-static rtos_tcb_t *current_task;
+static rtos_tcb_t _task_pool[RTOS_MAX_TASKS];
+static uint32_t _tsk_cnt;
+static rtos_tcb_t *_cur_task;
 static uint32_t _index = 0;
 static uint32_t _ticks = 0;
+
+static rtos_list_t _ready_l;
+static rtos_list_t _delayed_l;
+static rtos_list_t _delayed_overflow_l;
+static rtos_list_t _suspend_l;
 
 static _Alignas(8) rtos_stack_word_t idle_task_stack[RTOS_MIN_STACK_WORDS];
 
@@ -26,40 +32,40 @@ static rtos_tcb_t idle_task;
 static rtos_tcb_t *
 rtos_scheduler_select_next(rtos_stack_word_t *current_stack_pointer) {
   // Treat as start if NULL
-  if (current_task && current_task->state == RTOS_TASK_RUNNING)
-    current_task->state = RTOS_TASK_READY;
+  if (_cur_task && _cur_task->state == RTOS_TASK_RUNNING)
+    _cur_task->state = RTOS_TASK_READY;
 
   if (current_stack_pointer == NULL) {
     for (uint32_t i = 0U; i < RTOS_MAX_TASKS; i++) {
-      rtos_tcb_t *task = &task_pool[i];
+      rtos_tcb_t *task = &_task_pool[i];
       if (task->state == RTOS_TASK_READY) {
-        current_task = task;
-        current_task->state = RTOS_TASK_RUNNING;
+        _cur_task = task;
+        _cur_task->state = RTOS_TASK_RUNNING;
         _index = i;
-        return current_task;
+        return _cur_task;
       }
     }
 
     return NULL;
   }
 
-  current_task->stack_pointer = current_stack_pointer;
+  _cur_task->stack_pointer = current_stack_pointer;
   uint32_t i = _index;
   do {
     i = (i + 1) % RTOS_MAX_TASKS;
-    rtos_tcb_t *task = &task_pool[i];
+    rtos_tcb_t *task = &_task_pool[i];
     if (task->state == RTOS_TASK_READY) {
-      current_task = task;
-      current_task->state = RTOS_TASK_RUNNING;
+      _cur_task = task;
+      _cur_task->state = RTOS_TASK_RUNNING;
       _index = i;
-      return current_task;
+      return _cur_task;
     }
   } while (i != _index);
 
   // Return idle task as default if no task is schedulable
-  current_task = &idle_task;
-  current_task->state = RTOS_TASK_RUNNING;
-  return current_task;
+  _cur_task = &idle_task;
+  _cur_task->state = RTOS_TASK_RUNNING;
+  return _cur_task;
 };
 
 static inline void rtos_init_tcb(rtos_tcb_t *tcb, rtos_task_fn_t entry,
@@ -92,9 +98,9 @@ rtos_status_t rtos_task_create(rtos_task_fn_t entry, void *argument,
     return RTOS_ERROR_STACK_TOO_SMALL;
 
   for (uint32_t i = 0; i < RTOS_MAX_TASKS; i++) {
-    if (task_pool[i].state == RTOS_TASK_UNUSED) {
-      rtos_init_tcb(&task_pool[i], entry, argument, stack, stack_word_count);
-      task_count++;
+    if (_task_pool[i].state == RTOS_TASK_UNUSED) {
+      rtos_init_tcb(&_task_pool[i], entry, argument, stack, stack_word_count);
+      _tsk_cnt++;
       return RTOS_OK;
     }
   }
@@ -108,14 +114,19 @@ void rtos_system_init(void) {
                 RTOS_MIN_STACK_WORDS);
 
   for (uint32_t i = 0U; i < RTOS_MAX_TASKS; i++) {
-    task_pool[i] = (rtos_tcb_t){0};
-    task_pool[i].state = RTOS_TASK_UNUSED;
+    _task_pool[i] = (rtos_tcb_t){0};
+    _task_pool[i].state = RTOS_TASK_UNUSED;
   }
 
-  current_task = 0;
+  _cur_task = 0;
   _index = 0;
   _ticks = 0;
-  task_count = 0U;
+  _tsk_cnt = 0U;
+
+  rtos_init_list(&_ready_l);
+  rtos_init_list(&_delayed_l);
+  rtos_init_list(&_delayed_overflow_l);
+  rtos_init_list(&_suspend_l);
 
   rtos_port_scheduler_init();
 }
@@ -138,13 +149,13 @@ rtos_scheduler_switch_context(rtos_stack_word_t *current_stack_pointer) {
 }
 
 void rtos_block_current_task(uint32_t wake_tick, void *wait_obj) {
-  if (current_task == NULL)
+  if (_cur_task == NULL)
     return;
   rtos_port_irq_state_t prev_state = rtos_port_enter_critical();
-  current_task->wake_tick =
+  _cur_task->wake_tick =
       (wake_tick == RTOS_DELAY_INFINITY) ? RTOS_DELAY_INFINITY : wake_tick;
-  current_task->state = RTOS_TASK_BLOCKED;
-  current_task->wait_obj = wait_obj;
+  _cur_task->state = RTOS_TASK_BLOCKED;
+  _cur_task->wait_obj = wait_obj;
   rtos_port_exit_critical(prev_state);
 }
 
@@ -152,7 +163,7 @@ void rtos_unblock_task(uint32_t index) {
   if (index >= RTOS_MAX_TASKS)
     return;
 
-  rtos_tcb_t *task = &task_pool[index];
+  rtos_tcb_t *task = &_task_pool[index];
   if (task->state == RTOS_TASK_BLOCKED) {
     rtos_port_irq_state_t prev_state = rtos_port_enter_critical();
     task->state = RTOS_TASK_READY;
@@ -169,7 +180,7 @@ void rtos_tick_handler(void) {
 
   uint8_t need_switch = 0;
   for (uint32_t i = 0; i < RTOS_MAX_TASKS; i++) {
-    rtos_tcb_t *task = &task_pool[i];
+    rtos_tcb_t *task = &_task_pool[i];
     // Handle infinity and overflow in a later week when event + semaphore is
     // implemented and optimizing to ready list + wait list
     if (task->state == RTOS_TASK_BLOCKED && _ticks >= task->wake_tick)
@@ -184,4 +195,4 @@ void rtos_tick_handler(void) {
 
 uint32_t rtos_current_tick(void) { return _ticks; }
 
-const rtos_tcb_t *rtos_scheduler_current_task(void) { return current_task; }
+const rtos_tcb_t *rtos_scheduler_current_task(void) { return _cur_task; }
